cb454f07d80d49b75ee7a7ec5e33c369fc7d63db
ricardosribeiro <ricardo.sribeiro@outlook.com>
2023-04-23 19:50:43 -0300

    First Commit

---
+++ src/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "demo-aws-terraform-state-12345lskd"
+  force_destroy = true
+}
